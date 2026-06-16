7da60ccd7a4cd07b2e137e6ffc82f7efc36ec4af
VolareNJ <jc.h4@icloud.com>
2026-06-16 11:30:19 +0800

    封装成类/完成构造和析构函数/初步完成setsockopt

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 课程：B站LaoJ的C++进阶课程
 
 计划：将TCP制作为C++库
+
+进度：已实现setsockopt
---
+++ net.hpp
@@ -0,0 +1,53 @@
+﻿namespace net
+{
+    namespace clib
+    {
+#include<asm-generic/socket.h>
+#include<stdio.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
+    }
+
+    class socket
+    {
+    public:
+        socket(int domain, int type, int protocol)
+            : domain(domain)
+            , type(type)
+            , protocol(protocol)
+            , fd(clib::socket(domain, type, protocol))
+        {
+            if(fd == 0)
+            {
+                clib::perror("socket creation failed\n");
+                clib::exit(1);
+            }
+        }
+
+        ~socket()
+        {
+            clib::close(fd);
+        }
+
+        template<typename optival_t>
+        socket& set_opt(int level, int optname, optival_t optval)
+        {
+            if(clib::setsockopt(fd, level, optname, &optval, sizeof(optval)) == -1)
+            {
+                clib::perror("setsockopt failed\n");
+                clib::exit(1);
+            }
+            return *this;
+        }
+
+    private:
+        int domain;
+        int type;
+        int protocol;
+        int fd;
+    };
+
+}
