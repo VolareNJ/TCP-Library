ef3aa73ed64041d5e93a4d7b06452da2a17b45b4
VolareNJ <jc.h4@icloud.com>
2026-06-17 11:27:16 +0800

    实现配置/绑定/监听逻辑

--- net.hpp
+++ net.hpp
@@ -15,12 +15,10 @@
     {
     public:
         socket(int domain, int type, int protocol)
-            : domain(domain)
-            , type(type)
-            , protocol(protocol)
-            , fd(clib::socket(domain, type, protocol))
+            : m_param{ domain, type, protocol }
+            , m_fd(clib::socket(domain, type, protocol))
         {
-            if(fd == 0)
+            if(m_fd == 0)
             {
                 clib::perror("socket creation failed\n");
                 clib::exit(1);
@@ -29,13 +27,13 @@
 
         ~socket()
         {
-            clib::close(fd);
+            clib::close(m_fd);
         }
 
         template<typename optival_t>
         socket& set_opt(int level, int optname, optival_t optval)
         {
-            if(clib::setsockopt(fd, level, optname, &optval, sizeof(optval)) == -1)
+            if(clib::setsockopt(m_fd, level, optname, &optval, sizeof(optval)) == -1)
             {
                 clib::perror("setsockopt failed\n");
                 clib::exit(1);
@@ -43,11 +41,52 @@
             return *this;
         }
 
+        socket& set_addr(const int sin_family, const clib::in_addr sin_addr, const int sin_port)
+        {
+            m_addr.sin_family = sin_family;
+            m_addr.sin_addr = sin_addr;
+            int i = 1;
+            m_addr.sin_port = *(char*)&i == 1 ? clib::htons(sin_port) : sin_port;
+            return *this;
+        }
+
+        socket& bind()
+        {
+            bind(m_addr.sin_family, m_addr.sin_addr, m_addr.sin_port);
+            return *this;
+        }
+
+        socket& bind(const int sin_family, const clib::in_addr sin_addr, const int sin_port)
+        {
+            m_addr.sin_family = sin_family;
+            m_addr.sin_addr = sin_addr;
+            int i = 1;
+            m_addr.sin_port = *(char*)&i == 1 ? clib::htons(sin_port) : sin_port;
+            
+            if(clib::bind(m_fd, (clib::sockaddr*)&m_addr, sizeof(m_addr)) == -1)
+            {
+                clib::perror("bind failed\n");
+                clib::exit(1);
+            }
+
+            return *this;
+        }
+
+        socket& listen(int max_connections)
+        {
+            if(clib::listen(m_fd, max_connections) == -1)
+            {
+                clib::perror("listen failed\n");
+                clib::exit(1);
+            }
+            clib::printf("Server is listening on port %d...\n", m_addr.sin_port);
+            return *this;
+        }
+
     private:
-        int domain;
-        int type;
-        int protocol;
-        int fd;
+        int m_fd;
+        struct param_t { int domain; int type; int protocol; }m_param;
+        clib::sockaddr_in m_addr;
     };
 
 }
